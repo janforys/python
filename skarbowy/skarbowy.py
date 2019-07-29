70c2ded354c01ced906dc7fac625751ddff6e70c
Jan <janforys@wp.pl>
2019-07-29 17:18:16 +0200

    skarbowy

---
+++ skarbowy/skarbowy.py
@@ -0,0 +1,5 @@
+
+print("kwota zarobiona - ubezpieczenie spoleczne, zaokraglenie do pelnej zlotowki, pomnozenie razy procent podatku, zaokraglamy, odejmujemy ubezpieczenie zdrowotne do odliczenia")
+
+input()
+
