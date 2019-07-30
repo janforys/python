03ce3665000b05802c57f9f1c455b809782e650b
Jan <janforys@wp.pl>
2019-07-30 17:30:21 +0200

    skarbowy2

--- skarbowy/skarbowy.py
+++ skarbowy/skarbowy.py
@@ -1,5 +1,15 @@
-
-print("kwota zarobiona - ubezpieczenie spoleczne, zaokraglenie do pelnej zlotowki, pomnozenie razy procent podatku, zaokraglamy, odejmujemy ubezpieczenie zdrowotne do odliczenia")
+print("*************************************************\n kwota zarobiona minus ubezpieczenie spoleczne,\n zaokraglenie do pelnej zlotowki,\n pomnozenie razy procent podatku,\n zaokraglamy,\n odejmujemy ubezpieczenie zdrowotne do odliczenia\n*************************************************\n\n")
+print("Podaj zarobiona kwote: ")
 
 input()
 
+ubezp_zdrow_odl = 294.78
+ubezp_spol = 555.89
+procent_podatku = 0.085
+
+podatek = (7000 - ubezp_spol) * procent_podatku - ubezp_zdrow_odl
+
+print("Podatek do zaplaty wynosi:",podatek,"zl")
+
+print("\n")
+input("Wcisnij ENTER aby wyjsc :-)")
