6208ac3f18088cae52157119434aeda8b32c7354
Jan <janforys@wp.pl>
2019-07-31 16:55:27 +0200

    skarbowy3

--- skarbowy/skarbowy.py
+++ skarbowy/skarbowy.py
@@ -1,17 +1,30 @@
-print("*************************************************\n kwota zarobiona minus ubezpieczenie spoleczne,\n zaokraglenie do pelnej zlotowki,\n pomnozenie razy procent podatku,\n zaokraglamy,\n odejmujemy ubezpieczenie zdrowotne do odliczenia\n*************************************************\n\n")
+print("**************************************************\n kwota zarobiona odjac ubezpieczenie spoleczne,\n zaokraglenie do pelnej zlotowki,\n pomnozenie przez procent podatku,\n zaokraglamy,\n odejmujemy ubezpieczenie zdrowotne do odliczenia\n**************************************************\n\n")
 
-print("Podaj zarobiona kwote: ")
 
-val = input("Enter your value: ") 
-print(val) 
+kwota_zarobiona = float(input("Podaj zarobiona kwote (do 2och miejsc - uzyj kropki, a nie przecinka): "))
+print("TWOJA zarobiona kwota to:",kwota_zarobiona,"zl") 
 
 ubezp_zdrow_odl = 294.78
 ubezp_spol = 555.89
 procent_podatku = 0.085
 
-podatek = (7000 - ubezp_spol) * procent_podatku - ubezp_zdrow_odl
+# sprawdzamy
+a = kwota_zarobiona - ubezp_spol
+print(a)
+b = round(a)
+print(b)
+c = b * 0.085
+print(c)
+d = round(c)
+print(d)
+e = d - 294.78
+print(e)
+f = round(e, 2)
+print(f)
 
-print("Podatek do zaplaty wynosi:",podatek,"zl")
+podatek = round((round(kwota_zarobiona - ubezp_spol) * procent_podatku)) - ubezp_zdrow_odl
+
+print("Podatek do zaplaty wynosi:",round(podatek, 2),"zl")
 
 print("\n")
-input("Wcisnij ENTER aby wyjsc :-)")
+input("Nacisnij ENTER aby wyjsc :-)")
