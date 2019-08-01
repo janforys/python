4b7f8e3f36e0c10ef0daccaeb50da1b2215055ff
Jan <janforys@wp.pl>
2019-08-01 16:13:37 +0200

    skarbowy4

--- skarbowy/skarbowy.py
+++ skarbowy/skarbowy.py
@@ -1,8 +1,8 @@
-print("**************************************************\n kwota zarobiona odjac ubezpieczenie spoleczne,\n zaokraglenie do pelnej zlotowki,\n pomnozenie przez procent podatku,\n zaokraglamy,\n odejmujemy ubezpieczenie zdrowotne do odliczenia\n**************************************************\n\n")
+print("**************************************************\n kwota zarobiona odjac ubezpieczenie spoleczne,\n zaokraglenie,\n pomnozenie przez procent podatku,\n zaokraglamy,\n odejmujemy ubezpieczenie zdrowotne do odliczenia\n**************************************************\n\n")
 
 
 kwota_zarobiona = float(input("Podaj zarobiona kwote (do 2och miejsc - uzyj kropki, a nie przecinka): "))
-print("TWOJA zarobiona kwota to:",kwota_zarobiona,"zl") 
+print("TWOJA zarobiona kwota to:",kwota_zarobiona,"zl\n") 
 
 ubezp_zdrow_odl = 294.78
 ubezp_spol = 555.89
@@ -11,20 +11,20 @@ procent_podatku = 0.085
 # sprawdzamy
 a = kwota_zarobiona - ubezp_spol
 print(a)
-b = round(a)
+b = round(a, 2)
 print(b)
 c = b * 0.085
 print(c)
-d = round(c)
+d = round(c, 2)
 print(d)
 e = d - 294.78
 print(e)
 f = round(e, 2)
 print(f)
 
-podatek = round((round(kwota_zarobiona - ubezp_spol) * procent_podatku)) - ubezp_zdrow_odl
-
-print("Podatek do zaplaty wynosi:",round(podatek, 2),"zl")
+podatek = round(round(kwota_zarobiona - ubezp_spol, 2) * procent_podatku, 2) - ubezp_zdrow_odl
 
 print("\n")
+print("Podatek do zaplaty wynosi:",round(podatek, 2),"zl\n")
+
 input("Nacisnij ENTER aby wyjsc :-)")
