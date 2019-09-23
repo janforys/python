7e30dd38751cb2aa243b97a8d5d212e669a015ff
Jan <janforys@wp.pl>
2019-09-23 15:57:24 +0200

    pandyyyy

--- _Panda3D/_MOJE/MOJA_SCENA_1.py
+++ _Panda3D/_MOJE/MOJA_SCENA_1.py
@@ -15,27 +15,36 @@ class MyApp(ShowBase):
         self.scena1.setScale(0.10, 0.10, 0.10)
         self.scena1.setPos(-8, 42, 0)
 
-        # wstawiamy aktora
+        # wstawiamy aktora 1
         self.aktorPanda = Actor("models/panda-model",{"walk": "models/panda-walk4"})
         self.aktorPanda.setScale(0.005, 0.005, 0.005)
         self.aktorPanda.reparentTo(self.render)
 		
-		# animacja chodu
+		# animacja chodu 1
         self.aktorPanda.loop("walk")
-        przemieszczenie1 = self.aktorPanda.posInterval(5, Point3(0, -3, 0), startPos=Point3(0, 3, 0))
+        obrot3 = self.aktorPanda.hprInterval(3, Point3(30, 0, 0), startHpr=Point3(0, 3, 0))
+        przemieszczenie1 = self.aktorPanda.posInterval(5, Point3(3, -3, 0), startPos=Point3(0, 3, 0))
         obrot1 = self.aktorPanda.hprInterval(3, Point3(-180, 0, 0), startHpr=Point3(0, 0, 0))
-        przemieszczenie2 = self.aktorPanda.posInterval(5, Point3(0, 3, 0), startPos=Point3(0, -3, 0))
+        przemieszczenie2 = self.aktorPanda.posInterval(5, Point3(0, 3, 0), startPos=Point3(3, -3, 0))
         obrot2 = self.aktorPanda.hprInterval(3, Point3(0, 0, 0), startHpr=Point3(-180, 0, 0))
-        self.Idzie = Sequence(przemieszczenie1, obrot1, przemieszczenie2, obrot2)
+        self.Idzie = Sequence(obrot3, przemieszczenie1, obrot1, przemieszczenie2, obrot2)
         self.Idzie.loop()
         self.aktorPanda.setPos(0, 0, 0)
 
-        # wstawiamy drugiego aktora
+        # wstawiamy aktora 2
         self.aktorPanda2 = Actor("models/panda-model", {"walk": "models/panda-walk4"})
         self.aktorPanda2.setScale(0.01, 0.01, 0.01)
         self.aktorPanda2.reparentTo(self.render)
+
+        # animacja chodu 2
         self.aktorPanda2.loop("walk")
-        self.aktorPanda2.setPos(-6, 2, 0)
+        przemieszczenie1 = self.aktorPanda2.posInterval(10, Point3(-6, -6, 0), startPos=Point3(-6, 20, 0))
+        obrot1 = self.aktorPanda2.hprInterval(3, Point3(-180, 0, 0), startHpr=Point3(0, 0, 0))
+        przemieszczenie2 = self.aktorPanda2.posInterval(10, Point3(-6, 20, 0), startPos=Point3(-6, -6, 0))
+        obrot2 = self.aktorPanda2.hprInterval(3, Point3(0, 0, 0), startHpr=Point3(-180, 0, 0))
+        self.Idzie = Sequence(przemieszczenie1, obrot1, przemieszczenie2, obrot2)
+        self.Idzie.loop()
+        self.aktorPanda2.setPos(-6, 0, 0)
 
 app = MyApp()
 app.run()
