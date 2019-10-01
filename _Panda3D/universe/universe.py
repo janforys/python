b8e0a01442b3f42fedb65cf14ea008c2e8c52547
Jan <janforys@wp.pl>
2019-10-01 17:43:39 +0200

    universe_v1.05
    Rotate sun around itself and earth around the sun. LabelText added.

--- _Panda3D/universe/universe.py
+++ _Panda3D/universe/universe.py
@@ -1,14 +1,19 @@
 from direct.showbase.ShowBase import ShowBase
-base = ShowBase()
-
 from panda3d.core import *
 from direct.gui.DirectGui import *
+from direct.showbase.DirectObject import DirectObject
 
 import sys
 import gltf
 
+base = ShowBase()
 
 class World(object):
+
+    def genLabelText(self, text, i):
+        return  OnscreenText(text=text, pos=(0.06, -.06 * (i + 0.5)), fg=(1, 1, 1, 1),
+                            parent=base.a2dTopLeft,align=TextNode.ALeft, scale=.05)
+
     def __init__(self):
         self.title = OnscreenText(
             text="Universe",
@@ -48,13 +53,17 @@ class World(object):
 
         # Earth
         self.earth = loader.loadModel("meshes/earth.glb")
-        self.earth.reparentTo(render)
+        self.earth.reparentTo(self.orbit_root_earth)
         self.earth.setPos(10, 0, 0)
         self.earth.setScale(3.5 * self.sizescale)
         self.earth_tex = loader.loadTexture("textures/sky_stars_2.jpg")
         self.earth.setTexture(self.earth_tex, 1)
 
     def rotatePlanets(self):
+
+        # Rotate Sun
+        self.day_period_sun = self.sun.hprInterval(20, (360, 0 ,0))
+        self.day_period_sun.loop()
 
         # Rotate Earth
         self.orbit_period_earth = self.orbit_root_earth.hprInterval(
