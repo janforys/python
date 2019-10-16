6b60bb2724b55073bd5aab3198b0756289b8dcf3
Jan <janforys@wp.pl>
2019-10-16 19:03:20 +0200

    media-player_1.01

--- _Panda3D/media-player/media-player.py
+++ _Panda3D/media-player/media-player.py
@@ -19,4 +19,64 @@ def addTitle(text):
                         fg=(1, 1, 1, 1), shadow=(0, 0, 0, 1))
 
 
+class MediaPlayer(ShowBase):
 
+    def __init__(self, media_file):
+        ShowBase.__init__(self)
+
+        self.title = addTitle("Panda3D: MEDIA PLAYER")
+        self.instruction1 = addInstructions(0.06, "for Play/Pause press P")
+        self.instruction2 = addInstructions(0.12, "for Stop/Rewind press S")
+        self.instruction3 = addInstructions(0.18, "for Slow Motion/Normal press M")
+
+        # load movie texture and check it
+        self.tex = MovieTexture("name")
+        success = self.tex.read(media_file)
+        assert success, "Failed to load video!"
+
+        # fullscreen card set up to set the video texture
+        CM = CardMaker("My Fullscren Card")
+        CM.setFrameFullscreenQuad()
+
+        CM.setUvRange(self.tex)
+
+        # place the card in the scene graph and apply texture to it
+        card = NodePath(CM.generate())
+        card.reparentTo(self.render2d)
+        card.setTexture(self.tex)
+
+        self.sound = loader.loadSfx(media_file)
+        self.tex.synchronizeTo(self.sound)
+
+        self.accept('p', self.playpause)
+        #self.accept('P', self.playpause)
+        self.accept('m', self.fastforward)
+        #self.accept('M', self.fastforward)
+        self.accept('s', self.stopsound)
+        #self.accept('S', self.stopsound)
+
+    def stopsound(self):
+        self.sound.stop()
+        self.sound.setPlayRate(1.0)
+
+    def fastforward(self):
+        if self.sound.status() == AudioSound.PLAYING:
+            t = self.sound.getTime()
+            self.sound.stop()
+            if self.sound.getPlayRate() == 1.0:
+                self.sound.setPlayRate(0.5)
+            else:
+                self.sound.setPlayRate(1.0)
+            self.sound.setTime(t)
+            self.sound.play()
+
+    def playpause(self):
+        if self.sound.status() == AudioSound.PLAYING:
+            t = self.sound.getTime()
+            self.sound.stop()
+            self.sound.setTime(t)
+        else:
+            self.sound.play()
+
+player = MediaPlayer("benek.mp4")
+player.run()
