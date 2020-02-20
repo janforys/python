d5e83065bf466243d558df6864d8eea23137e5a3
Jan Foryś <janforys@wp.pl>
2020-02-20 18:45:45 +0100

    Update skarbowy.py

--- skarbowy/skarbowy.py
+++ skarbowy/skarbowy.py
@@ -4,8 +4,8 @@ print("**************************************************\n kwota zarobiona odja
 kwota_zarobiona = float(input("Podaj zarobiona kwote (do 2och miejsc - uzyj kropki, a nie przecinka): "))
 print("TWOJA zarobiona kwota to:",kwota_zarobiona,"zl\n") 
 
-ubezp_zdrow_odl = 294.78
-ubezp_spol = 555.89
+ubezp_zdrow_odl = 312.02
+ubezp_spol = 609.14
 procent_podatku = 0.085
 
 # sprawdzamy
@@ -13,15 +13,16 @@ a = kwota_zarobiona - ubezp_spol
 print(a)
 b = round(a, 2)
 print(b)
-c = b * 0.085
+c = b * procent_podatku
 print(c)
 d = round(c, 2)
 print(d)
-e = d - 294.78
+e = d - ubezp_zdrow_odl
 print(e)
 f = round(e, 2)
 print(f)
 
+# liczymy xD
 podatek = round(round(kwota_zarobiona - ubezp_spol, 2) * procent_podatku, 2) - ubezp_zdrow_odl
 
 print("\n")
